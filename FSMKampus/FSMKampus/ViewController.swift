c92f2809045131c6aa1c5d318f167a385e0afb34
Mert AGCAKOYUN <mert.agcakoyun@stu.fsm.edu.tr>
2018-12-06 19:24:46 +0300

    secondCommit

---
+++ FSMKampus/FSMKampus/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  FSMKampus
+//
+//  Created by Mert Agcakoyun on 6.12.2018.
+//  Copyright © 2018 Mert Agcakoyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+  
+    
+
+    
+    
+    
+   
+}
+
